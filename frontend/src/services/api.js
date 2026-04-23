4b9fd22f8eeb6d0c0687b1ec35f54fab73c91a21
Vardhan158 <harshavardhandevang@gmail.com>
2026-04-23 21:00:39 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://attendance-tracker-1-xove.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
